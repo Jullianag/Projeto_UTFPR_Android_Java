b57d38c26a547e3b1038f0ae0257f6535a184894
Jullianag <juh_g@hotmail.com>
2025-07-31 16:58:09 -0300

    Adicionando funcionalidade aos botoes Salvar e Limpar

--- app/src/main/java/br/edu/utfpr/colecaojogovideogame/MainActivity.java
+++ app/src/main/java/br/edu/utfpr/colecaojogovideogame/MainActivity.java
@@ -1,7 +1,9 @@
 package br.edu.utfpr.colecaojogovideogame;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,5 +18,55 @@ public class MainActivity extends AppCompatActivity {
 
         editTextNome = findViewById(R.id.editTextNome);
         editTextAno = findViewById(R.id.editTextAno);
+    }
+
+    public void limparCampos(View view) {
+
+        editTextNome.setText(null);
+        editTextAno.setText(null);
+
+        editTextNome.requestFocus();
+
+        Toast.makeText(this,
+                R.string.entradas_apagadas,
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void salvarValores(View view) {
+
+        String nome = editTextNome.getText().toString();
+
+        if (nome == null || nome.trim().isEmpty()) {
+
+            Toast.makeText(this,
+                    R.string.o_nome_nao_pode_ser_vazio,
+                    Toast.LENGTH_LONG).show();
+
+            editTextNome.requestFocus();
+            return;
+        }
+
+        nome = nome.trim();
+
+        String anoString = editTextAno.getText().toString();
+
+        if (anoString == null || anoString.trim().isEmpty()) {
+
+            Toast.makeText(this,
+                    R.string.o_ano_nao_pode_ser_vazio,
+                    Toast.LENGTH_LONG).show();
+
+            editTextAno.requestFocus();
+            return;
+        }
+
+        int ano = 0;
+
+        ano = Integer.parseInt(anoString);
+
+        Toast.makeText(this,
+                getString(R.string.jogo_valor) + nome + "\n" +
+                getString(R.string.ano_valor) + ano,
+                Toast.LENGTH_LONG).show();
     }
 }
