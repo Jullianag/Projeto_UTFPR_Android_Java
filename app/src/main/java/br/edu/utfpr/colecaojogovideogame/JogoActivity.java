e78e6f0d7040c5f22f3c9d5b2d5db4afc0f640eb
Jullianag <juh_g@hotmail.com>
2025-08-01 12:59:37 -0300

    Validacao do campo Ano

--- app/src/main/java/br/edu/utfpr/colecaojogovideogame/JogoActivity.java
+++ app/src/main/java/br/edu/utfpr/colecaojogovideogame/JogoActivity.java
@@ -1,5 +1,6 @@
 package br.edu.utfpr.colecaojogovideogame;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -7,14 +8,17 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import java.time.LocalDate;
+import java.util.Calendar;
+
+public class JogoActivity extends AppCompatActivity {
 
     private EditText editTextNome, editTextAno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_jogo);
 
         editTextNome = findViewById(R.id.editTextNome);
         editTextAno = findViewById(R.id.editTextAno);
@@ -62,7 +66,41 @@ public class MainActivity extends AppCompatActivity {
 
         int ano = 0;
 
-        ano = Integer.parseInt(anoString);
+        try {
+            ano = Integer.parseInt(anoString);
+
+        } catch (NumberFormatException e) {
+
+            Toast.makeText(this,
+                    R.string.o_ano_deve_ser_um_numero_inteiro,
+                    Toast.LENGTH_LONG).show();
+
+            editTextAno.requestFocus();
+            editTextAno.setSelection(0, editTextAno.getText().toString().length());
+            return;
+        }
+
+        int anoAtual;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            anoAtual = LocalDate.now().getYear();
+        }
+        else {
+            // tive que usar o Calendar, pois o andoid nao aceitava o LocalDate
+            anoAtual = Calendar.getInstance().get(Calendar.YEAR);
+        }
+
+        if (ano < 1972 || ano > anoAtual) {
+
+            Toast.makeText(this,
+                    R.string.o_ano_deve_estar_entre_1972_e_o_ano_atual,
+                    Toast.LENGTH_LONG).show();
+
+            editTextAno.requestFocus();
+            editTextAno.setSelection(0, editTextAno.getText().toString().length());
+            return;
+        }
 
         Toast.makeText(this,
                 getString(R.string.jogo_valor) + nome + "\n" +
